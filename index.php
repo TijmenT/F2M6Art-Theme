c17dd4ca68ce908a74baa41d523ac6b2cc089f21
Tijmen <30957@ma-web.nl>
2021-12-02 10:54:31 +0100

    eerste commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    get_header();
+
+    $args = [
+        'header-menu' => ( 'Header Menu' ),
+        'link_after' => ( '*' )
+    ];
+    wp_nav_menu($args);
+?>
+
+<?php
+
+
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+
+    <h2><?php the_title() ?></h2>
+    <?php the_content() ?>
+    <a href="<?php the_permalink()?>">Lees Meer</a>
+    <hr>
+
+    <?php endwhile;
+
+else :
+    echo '<p> there are no posts!</p>';
+
+endif;
+
+?>
