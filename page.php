f7a84b9a39e7e2fb4d9071a7475e1423693c9292
Amyvd <70899521+Amyvd@users.noreply.github.com>
2022-01-13 00:44:28 +0100

    opdracht 16 af

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+    get_header();
+
+    $args = [
+        'header-menu' => ( 'header menu' ),
+        'link_before' => ( ''),
+        'link_after' => ( '' ),
+        'menu' => ('navbar'),
+        '.menu-item' => ('navbarMenu'), '.menu-item-has-children' => ('page-item-29, page-item-33, page-item-24')
+    ];
+    
+?>
+
+<?php
+
+
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post(); ?>
+    <h2><?php the_title() ?></h2>
+    <?php the_content() ?>
+    <hr>
+
+    <?php endwhile;
+
+else :
+    echo '<p> there are no posts!</p>';
+
+endif;
+
+    get_footer();
+
+?>
